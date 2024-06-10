7f55d8b9b87cc3af4f763f5a62715a3a18b1ecb1
oufkirhamza <oufkirhamza08@gmail.com>
2024-06-10 11:11:30 +0100

    notif modification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,53 +67,6 @@
                             <div id="notifications">
 
                             </div>
-                            {{-- @foreach ($reviews as $review)
-                                <a href="{{ route('notif.show', $review) }}" class="no-underline">
-                                    <div
-                                        class="{{ $review->mark_read ? 'bg-white' : ' bg-indigo-100' }} w-full relative p-2 rounded flex gap-1 cursor-pointer no-underline decoration-black text-black">
-                                        <div
-                                            class="{{ $review->status == 'alert' ? 'bg-red-700' : ($review->status == 'satisfying' ? 'bg-emerald-700' : 'bg-amber-500') }} w-2 absolute left-0 top-0 h-[4.9rem]  rounded-l">
-                                        </div>
-                                        <div class="w-full px-2">
-                                            <div class="flex justify-between items-center w-full">
-                                                <p class="mb-0">
-                                                    <span class="font-bold">Lionel Messi</span> added a review for
-                                                    <span class="font-bold">Mahkama</span>
-                                                </p>
-                                                <div class="flex justify-start items-center ">
-                                                    <form action="{{ route('update.notif', $review) }}" method="POST">
-                                                        @csrf
-                                                        @method('PUT')
-                                                        <button
-                                                            class="p-1 hover:bg-gray-100 hover:p-1 hover:rounded-sm">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                viewBox="0 0 24 24" stroke-width="1.5"
-                                                                stroke="currentColor" class="size-3 cursor-pointer">
-                                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                                    d="m4.5 12.75 6 6 9-13.5" />
-                                                            </svg>
-                                                        </button>
-                                                    </form>
-                                                    <form action="{{ route('notif.delete', $review) }}" method="post">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button
-                                                            class=" p-1 hover:bg-gray-100 hover:p-1 hover:rounded-sm">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                viewBox="0 0 24 24" stroke-width="1.5"
-                                                                stroke="currentColor" class="size-3 cursor-pointer">
-                                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                                    d="M6 18 18 6M6 6l12 12" />
-                                                            </svg>
-                                                        </button>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                            <p class="truncate max-w-[20vw]">{{ $review->content }}</p>
-                                        </div>
-                                    </div>
-                                    </a>
-                            @endforeach --}}
                         </div>
                     </div>
                     <div class="hidden sm:flex {{ isset($header) ? '' : 'sm:ml-auto' }}">

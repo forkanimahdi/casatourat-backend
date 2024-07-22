3a4bab202f15c13adee7b8d2468b5e7e292c23ae
forkanimahdi <forkanimahdi@gmail.com>
2024-07-22 14:54:25 +0100

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/css/app.css',
+                'resources/js/assign_building_map.js',
+                'resources/js/create_circuit_map.js',
+                'resources/js/show_circuit_map.js',
+                'resources/js/update_circuit_map.js',
+                'resources/js/search.js'
             ],
             refresh: true,
         }),

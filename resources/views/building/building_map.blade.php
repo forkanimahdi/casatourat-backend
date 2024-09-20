98853fa8746f89e7405264024798ca1068c8b9c1
oufkirhamza <oufkirhamza08@gmail.com>
2024-09-20 16:58:27 +0100

    create building view

--- resources/views/building/building_map.blade.php
+++ resources/views/building/building_map.blade.php
@@ -6,8 +6,134 @@
             </h2>
         </div>
     </x-slot>
-    <div id="map" class="w-full h-[88vh]">
+    <div class="flex gap-3 bg-[#ffffffb2]">
+        {{-- mapppppppp --}}
+        <div id="map" class="w-[60%] h-[85vh] ">
+        </div>
+        <div class="w-[40%] p-4 ">
+            <h5>Create Building</h5>
+            <div id="tabsBtn" class="flex bg-gray-200 w-full justify-between gap-2 p-1 rounded-lg">
+                <button id="btnEnglish" type="button"
+                    class="w-1/3 rounded-md font-medium p-1 langueBtn">English</button>
+                <button id="btnFrench" type="button"
+                    class="w-1/3 rounded-md font-medium p-1 langueBtn">Français</button>
+                <button id="btnArabic" type="button"
+                    class="w-1/3 rounded-md font-medium p-1 langueBtn">العربية</button>
+            </div>
+            <form action="{{ route('building.store') }}" class="flex flex-col gap-2 p" method="post"
+                enctype="multipart/form-data">
+                @csrf
+                <div id="english_version" class="block">
+                    <div class="flex flex-col w-full">
+                        <label for="" class="w-full font-bolder text-base">Name</label>
+                        <input class="rounded w-full" type="text" placeholder="name" name="name">
+                    </div>
+                    <div class="flex flex-col w-full ">
+                        <label for="" class="w-full font-bolder text-base">Description</label>
+                        <input class="rounded w-full" type="text" placeholder="description" name="description">
+                    </div>
+                    <span>Audio</span>
+                    <div
+                        class="flex flex-col py-2 cursor-pointer items-center border-[1.5px] border-gray-500 rounded w-full">
+                        <label for="audio" class="w-full font-bolder text-base flex gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                            </svg>
+                            Upload Audio
+                        </label>
+                        <input id="audio" class="rounded w-full d-none" type="file" placeholder="audio"
+                            name="audio" accept="audio/*">
+                    </div>
+                </div>
+                <div id="french_version" class="hidden">
+                    <div class="flex flex-col w-full">
+                        <label for="" class="w-full font-bolder text-base">Nom</label>
+                        <input class="rounded w-full" type="text" placeholder="Etrez Nom" name="name">
+                    </div>
+                    <div class="flex flex-col w-full ">
+                        <label for="" class="w-full font-bolder text-base">Description</label>
+                        <input class="rounded w-full" type="text" placeholder="description" name="description">
+                    </div>
+                    <span>Audio</span>
+                    <div
+                        class="flex flex-col py-2 cursor-pointer items-center border-[1.5px] border-gray-500 rounded w-full">
+                        <label for="audio" class="w-full font-bolder text-base flex gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                            </svg>
+                            Upload Audio
+                        </label>
+                        <input id="audio" class="rounded w-full d-none" type="file" placeholder="audio"
+                            name="audio" accept="audio/*">
+                    </div>
+                </div>
+                <div id="arabic_version" class="hidden">
+                    <div class="flex flex-col text-right w-full">
+                        <label for="" class="w-full font-bolder text-base">الاسم</label>
+                        <input class="rounded w-full text-right" type="text" placeholder="الاسم" name="name">
+                    </div>
+                    <div class="flex flex-col text-right w-full ">
+                        <label for="" class="w-full font-bolder text-base">الوصف</label>
+                        <input class="rounded w-full text-right" type="text" placeholder="الوصف" name="description">
+                    </div>
 
+                    <p class="text-right m-0">الصوت</p>
+                    <div
+                        class="flex flex-col  py-2 cursor-pointer items-center border-[1.5px] border-gray-500 rounded w-full">
+                        <label for="audio" class="w-full font-bolder items-end flex-row-reverse text-base flex gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                            </svg>
+                            Upload Audio
+                        </label>
+                        <input id="audio" class="rounded w-full d-none" type="file" placeholder="audio"
+                            name="audio" accept="audio/*">
+                    </div>
+                </div>
+
+                <div class="flex flex-col w-full gap-2 ">
+                    <label class="w-full font-bolder text-base" for="latitude">Latitude</label>
+                    <input type="text" class="rounded w-full" id="latitude" placeholder="latitude" name="latitude">
+                </div>
+                <div class="flex flex-col w-full ">
+                    <label class="w-full font-bolder text-base" for="longitude">Longtitude</label>
+                    <input type="text" class="rounded w-full" id="longitude" placeholder="longitude"
+                        name="longitude">
+                </div>
+                <div class="flex flex-col gap-2">
+                    <span>Images</span>
+                    <div
+                        class="flex flex-col py-2 cursor-pointer items-center border-[1.5px] border-gray-500 rounded w-full">
+                        <label for="image" class="w-full font-bolder text-base flex gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                            </svg>
+                            Upload Image
+                        </label>
+                        <input id="image" class="rounded w-full d-none" type="file" placeholder="image" multiple
+                            name="image[]" accept="image/*">
+                    </div>
+                </div>
+                <button
+                    class="flex items-center justify-center gap-2 p-2 bg-alpha text-gray-100 rounded w-full font-bold hover:shadow hover:border-2 hover:border-alpha hover:text-alpha hover:bg-gray-100 duration-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                    </svg>
+                    Create Building</button>
+            </form>
+        </div>
     </div>
 
     @include('building.partials.add_building_modal')
@@ -38,7 +164,7 @@
             const map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 10,
                 center: casablanca,
-                mapTypeId: google.maps.MapTypeId.HYBRID
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
             });
 
 
@@ -78,6 +204,7 @@
             });
 
             map.addListener('click', function(event) {
+
                 const marker = new google.maps.Marker({
                     position: {
                         lat: event.latLng.lat(),
@@ -90,7 +217,7 @@
                         anchor: new google.maps.Point(20, 45),
                     }
                 });
-                document.getElementById('submit').click()
+                // document.getElementById('submit').click()
 
                 const modal = document.getElementById('staticBackdrop');
 
--- resources/views/building/partials/update_building_tab.blade.php
+++ resources/views/building/partials/update_building_tab.blade.php
@@ -1,139 +1,15 @@
-<div class="hidden px-4 py-2  flex-col gap-3 justify-center ">
-    <div class="">
-        {{-- TODO: Delete function for a building --}}
-        <div class="flex justify-between items-center ">
-            <h3>Update building info :</h3>
-            @include('building.partials.confirmation_modale')
-        </div>
-        <form action="{{ route('building.update', $building) }}" method="post" enctype="multipart/form-data"
-            class="w-full flex flex-col gap-4">
-            @csrf
-            @method('PUT')
-            <div class="flex flex-col gap-1">
-                <label for="" class='font-medium'>Buinding name</label>
-                <input class="rounded-md " type="text" required value="{{ $building->name }}" placeholder="name"
-                    name="name">
-            </div>
-            <div class="flex flex-col gap-1">
-                <label for="" class='font-medium '>Building description</label>
-                <textarea class="rounded-md" name="description" placeholder="description" type="text" id="" cols="30"
-                    rows="5" required>{{ $building->description }}</textarea>
-            </div>
-            <div class="flex  gap-4">
-                <label for="Build-audio"
-                    class='font-bolder text-base px-3 py-2 cursor-pointer text-gray-100 bg-alpha hover:text-alpha hover:bg-gray-100 border-2 border-alpha  rounded-md flex items-center gap-2'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-                    </svg>
-                    Change Audio
-                </label>
-                <input class="rounded-md hidden" id="Build-audio" type="file" placeholder="audio" name="audio">
-                <audio controls class=" w-1/2" id="audio-preview">
-                    <source src="{{ asset('storage/audios/' . $building->audio) }}">
-                </audio>
-            </div>
-            <div class="flex flex-col py-2 px-3">
-                <label class="block text-gray-700" for="addImage">Add an Image: </label>
-                <input multiple name="image[]" type="file" id="addImage" accept="image/png, image/jpeg" multiple
-                    class="mt-2 border-2 rounded w-full bg-white">
-            </div>
-            <div class="w-full flex justify-center">
-                <button type="submit"
-                    class="px-4 py-2 font-medium text-lg text-gray-100 bg-alpha hover:text-alpha hover:bg-gray-100 border-2 border-alpha rounded-md ">Update
-                    informations</button>
-            </div>
-        </form>
-        {{-- update images --}}
-        <div class="mt-5">
-            <h3>Update building Images :</h3>
-            <div class="p-2 flex flex-wrap ">
-                @foreach ($building->images as $index => $image)
-                    <div class="p-2 flex justify-between relative  h-[50%] w-1/3" x-data="{
-                        imagePreview: '{{ asset('storage/images/' . $image->path) }}',
-                        file: null,
-                        hasImagePreview: {{ $image->path ? 'false' : 'true' }}
-                    }">
-
-                        <!-- Display the image preview -->
-                        <img :src="imagePreview"
-                            class="rounded-md w-full object-cover object-center selected-img aspect-square"
-                            alt="">
-
-                        <div class="flex gap-2 items-center absolute top-[2.5vh] right-[1.5vw]">
-                            {{-- Form for updating the image --}}
-                            <form action="{{ route('building.update_image', $image) }}" class="flex items-center gap-2"
-                                id="update_image_{{ $index }}" enctype="multipart/form-data" method="post">
-                                @csrf
-                                @method('PUT')
-
-                                <label for="image-build_{{ $index }}"
-                                    class="cursor-pointer p-2 font-semibold text-gray-100 no-underline bg-alpha rounded-lg shadow-md hover:shadow-lg hover:text-alpha hover:bg-gray-100 border-2 border-alpha transition ">Change
-                                    Image</label>
-
-                                <input id="image-build_{{ $index }}" class="hidden" type="file" name="image"
-                                    accept="image/png, image/jpeg"
-                                    @change="file = $event.target.files[0];
-                                    if (file) {
-                                        const reader = new FileReader();
-                                        reader.onload = (e) => imagePreview = e.target.result;
-                                        reader.readAsDataURL(file);
-                                        hasImagePreview = true;
-                                    }">
-
-                                <button form="update_image_{{ $index }}"
-                                    :class="{ 'bg-green-500': hasImagePreview, 'hidden': !hasImagePreview }"
-                                    class="cursor-pointer p-2 font-semibold text-gray-100 no-underline rounded-lg shadow-md ">
-
-                                    <svg viewBox="0 0 24 24" fill="none" class="h-5 w-5"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.5 12.5L10.5 14.5L15.5 9.5" stroke="#FFFFFF" stroke-width="1.5"
-                                            stroke-linecap="round" stroke-linejoin="round" />
-                                        <path
-                                            d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7"
-                                            stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" />
-                                    </svg>
-                                </button>
-                            </form>
-
-                            {{-- Form for deleting the image --}}
-                            <form class="flex justify-end" action="{{ route('building.destory_image', $image) }}"
-                                method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button
-                                    class="cursor-pointer p-2 font-semibold text-gray-100 no-underline bg-red-500 rounded-lg shadow-md hover:shadow-lg hover:text-red-500 hover:bg-gray-100 border-2 border-red-500 transition duration-200 ">
-
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
-                                        fill="currentColor">
-                                        <path
-                                            d="M8 3V2h4v1h5v2H3V3h5zm1 0h2V2H9v1zM4 6h12v10a2 2 0 01-2 2H6a2 2 0 01-2-2V6zm2 0v10h8V6H6z" />
-                                    </svg>
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                @endforeach
-
-            </div>
-        </div>
-    </div>
-</div>
 <div>
     <div class="flex justify-between min-h-screen gap-2 ">
         <div class="w-[60%] flex flex-col gap-3 border rounded-lg p-4 bg-white">
             <h1>Building Details</h1>
             <div id="tabsBtn" class="flex bg-gray-200 w-full justify-between gap-2 p-1 rounded-lg">
-                <button id="btnEnglish" type="button"
-                    class="w-1/3 rounded-md font-medium p-1 langueBtn">English</button>
-                <button id="btnFrench" type="button"
-                    class="w-1/3 rounded-md font-medium p-1 langueBtn">Français</button>
+                <button id="btnEnglish" type="button" class="w-1/3 rounded-md font-medium p-1 langueBtn">English</button>
+                <button id="btnFrench" type="button" class="w-1/3 rounded-md font-medium p-1 langueBtn">Français</button>
                 <button id="btnArabic" type="button"
                     class="w-1/3 rounded-md font-medium p-1 langueBtn">العربية</button>
             </div>
-            <form id="english_version" class="block" action="{{ route('building.update', $building) }}"
-                method="post" enctype="multipart/form-data">
+            <form id="english_version" class="block" action="{{ route('building.update', $building) }}" method="post"
+                enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
                 <div class="flex flex-col gap-2">
@@ -143,8 +19,8 @@
                 </div>
                 <div class="flex flex-col gap-2">
                     <label class="font-medium" for="desc">Building Description</label>
-                    <textarea class="rounded-md" name="description" placeholder="Enter building description" id="desc"
-                        cols="30" rows="10" required>{{ $building->description }}</textarea>
+                    <textarea class="rounded-md" name="description" placeholder="Enter building description" id="desc" cols="30"
+                        rows="10" required>{{ $building->description }}</textarea>
                 </div>
                 <div class="flex flex-col gap-2 ">
                     <label class="font-medium">Audio Description</label>
@@ -168,8 +44,7 @@
                         Information</button>
                 </div>
             </form>
-            <form id="french_version" class="hidden" action=""
-                method="post" enctype="multipart/form-data">
+            <form id="french_version" class="hidden" action="" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
                 <div class="flex flex-col gap-2">
@@ -195,7 +70,7 @@
                         </label>
                         <input class="rounded-md hidden" id="Build-audio" type="file" placeholder="audio"
                             name="audio"> <audio controls class=" w-full" id="audio-preview">
-                            <source src="{{ asset('storage/audios/' . $building->audio) }}">
+                            <source src="">
                         </audio>
                     </div>
                 </div>
@@ -204,8 +79,7 @@
                         Information</button>
                 </div>
             </form>
-            <form id="arabic_version" class="hidden" action=""
-                method="post" enctype="multipart/form-data">
+            <form id="arabic_version" class="hidden" action="" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
                 <div class="flex flex-col text-right gap-2">
@@ -231,7 +105,7 @@
                         </label>
                         <input class="rounded-md hidden" id="Build-audio" type="file" placeholder="audio"
                             name="audio"> <audio controls class=" w-full" id="audio-preview">
-                            <source src="{{ asset('storage/audios/' . $building->audio) }}">
+                            <source src="">
                         </audio>
                     </div>
                 </div>

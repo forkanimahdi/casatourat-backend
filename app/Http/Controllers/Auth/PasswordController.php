e532fea8bdb881022d77f5a8172aa43dbc67aedc
youness <younessaithadou9@gmail.com>
2024-10-16 11:22:08 +0100

    change admin password on clerk

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Visitor;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\Rules\Password;
 
 class PasswordController extends Controller
@@ -20,10 +23,26 @@ class PasswordController extends Controller
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
 
+        //get auth user token
+        $userToken = Visitor::where('email', Auth::user()->email)->value('token');
+        if (!$userToken)
+            return back()->with('error', "admin account not found");
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . config("clerk.secret_key"),
+            'Content-Type' => 'application/json'
+        ])->patch('https://api.clerk.com/v1/users/' . $userToken, [
+            "password" => $request->password
+        ]);
+
+        if (!$response->ok()) {
+            return back()->with('error', "error while trying to udpdate password please try again.");
+        }
+
         $request->user()->update([
             'password' => Hash::make($validated['password']),
         ]);
 
-        return back()->with('status', 'password-updated');
+        return back()->with('success', 'password updated');
     }
 }

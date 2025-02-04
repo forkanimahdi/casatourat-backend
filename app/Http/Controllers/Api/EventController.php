e88f0b86539a1befeb3cfdce08dd4564410f008a
osama-jeb <oussamajebrane98@gmail.com>
2025-02-04 11:51:35 +0100

    event guest

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -12,51 +12,14 @@ class EventController extends Controller
 {
     use TokenValidation;
 
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    public function getAudio()
-    {
-        $audio = asset('storage/audios/1727880387_en.mp3');
-        return response()->json($audio);
-    }
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
-     * Display the specified resource.
+     * Send the specified resource.
      */
     public function show(Request $request)
     {
-        return $this->validateToken(
-            $request,
-            fn() => EventResource::collection(Event::orderBy('created_at', 'desc')->get())
-        );
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        // Send all the events
+        $events = Event::all();
+        return EventResource::collection($events);
     }
 }
